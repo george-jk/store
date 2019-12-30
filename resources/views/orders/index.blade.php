9f0b768763d5c7ed9abef3bea75f6bf4fdc7a225
george-jk <georgikondev@gmail.com>
2019-12-30 15:35:43 +0200

    modified:   resources/views/orders/index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -5,7 +5,7 @@
 	<ul class="list-group list-group-flush">
 		@foreach($orders as $order)
 		<li class="list-group-item list-group-item-action">	
-			<a href="{{route('orders.show',$order->id)}}" class="list-group-item-action">{{$order->created_at}} ~ {{$order->status()->latest()->first()->status}}</a>
+			<a href="{{route('orders.show',$order->id)}}" class="list-group-item-action">{{$order->created_at}} ~ {{$order->status()->orderby('created_at','desc')->first()->status}}{{-- {{$order->status()->latest()->first()->ststus}} --}}</a>
 		</li>	
 		@endforeach
 	</ul>
