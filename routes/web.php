7b23cc27775c521a27577aa2ef7b8e1e4a085d45
george-jk <georgikondev@gmail.com>
2020-02-25 23:02:32 +0200

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Auth::routes([
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'HomeController@index');
-
 Route::prefix('admin')->group(function () {
 	Route::resource('orders','OrderController')->middleware('verified');
 	Route::post('orders/status-change/{order}','OrderController@changeStatus')->middleware('verified')->name('orders.status-change');
